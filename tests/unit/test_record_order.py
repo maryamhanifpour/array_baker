199efde3d644d97c9106c0b5cca1418d672c8f0d
SaraHanifpour <maryam.hanifpour@gmail.com>
2020-09-28 10:41:37 +0300

    added record order test

--- tests/unit/test_record_order.py
+++ tests/unit/test_record_order.py
@@ -5,22 +5,14 @@ import array_maker
 
 path = ['/Users/maryam/Desktop/sample/sample_string.txt', '/Users/maryam/Desktop/sample/sample_string3.txt']
 
-@pytest.mark.parametrize("path", path)
-def test_record_intact(path):
-    sample = array_maker.sample_reader(path)
-    sample_bacthes = array_maker.bake_batch(path)
-    records = list(itertools.chain(*sample_bacthes))
-    distorted_records = len([r for r in records if r not in sample])
-    assert distorted_records==0
-
 
 
 @pytest.mark.parametrize("path", path)
-def test_record_number_intactx(path):
+def test_record_order(path):
     sample = array_maker.sample_reader(path)
     sample_bacthes = array_maker.bake_batch(path)
     records = list(itertools.chain(*sample_bacthes))
     filtered = array_maker.process.size_filter(sample)
-    assert len(filtered) == len(records)
+    assert filtered == records
 
 
